bebd2816854f5bc0b118eae5939f33597ea4ba76
AlexaDickenson <127786567+AlexaDickenson@users.noreply.github.com>
2023-04-17 12:42:41 -0400

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,8 @@
+
+const getHeader= document.getElementsByTagName('header');
+const getSection= document.getElementsByTagName('section');
+const getCurrent= document.getElementsByClassName('current');
+const getNext = getCurrent.nextElementSibling;
+const getH2 = prevSection.querySelector('h2');
+const getHighlight= getCurrendocument.querySelector('h2.highlight');
+const getAllH2= document.querySelectorAll('h2')
